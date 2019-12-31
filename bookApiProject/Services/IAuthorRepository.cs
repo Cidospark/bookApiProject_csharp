8a029ee38106045c6579a2fa8fb30db89274cc48
cidospark <cidospark27@gmail.com>
2019-12-31 16:54:36 +0100

    added reviewer repository interface

--- bookApiProject/Services/IAuthorRepository.cs
+++ bookApiProject/Services/IAuthorRepository.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using bookApiProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace bookApiProject.Services
 {
-    interface IAuthorRepository
+    public interface IAuthorRepository
     {
+        ICollection<Author> GetAuthors();
+        Author GetAuthor(int authorId);
+        ICollection<Author> GetAuthorsOfABook(int bookId);
+        ICollection<Book> GetBooksByAuthor(int authorId);
+        bool AuthorExists(int authorId);
     }
 }
---
+++ bookApiProject/Services/IReviewerRepository.cs
@@ -0,0 +1,17 @@
+﻿using bookApiProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookApiProject.Services
+{
+    public interface IReviewerRepository
+    {
+        ICollection<Reviewer> GetReviewers();
+        Reviewer GetReviewers(int reviewerId);
+        ICollection<Review> GetReviewsByReviewer(int reviewerId);
+        ICollection<Reviewer> GetReviewerOfAReview(int reviewId);
+        bool ReviewerExists(int reviewerId);
+    }
+}
