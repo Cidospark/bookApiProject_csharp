028695c3db87fde2d22cc047c7f77bc55124adca
cidospark <cidospark27@gmail.com>
2019-12-31 17:25:08 +0100

    added book repository interface

--- bookApiProject/Services/IBookRepository.cs
+++ bookApiProject/Services/IBookRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using bookApiProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,15 @@ namespace bookApiProject.Services
 {
     interface IBookRepository
     {
+        ICollection<Book> GetBooks();
+        Book GetBook(int bookId);
+        Book GetBook(string isbn);
+        bool BookExists(int bookId);
+        bool BookExists(string isbn);
+
+        bool IsDuplicate(int bookId);
+
+        double GetBookRating(int bookId);
+
     }
 }
