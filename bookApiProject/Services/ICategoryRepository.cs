c1b2b1c985190a7c14e9d68e01ade14559cd79b4
cidospark <cidospark27@gmail.com>
2019-12-31 09:38:28 +0100

    added category interface

---
+++ bookApiProject/Services/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using bookApiProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookApiProject.Services
+{
+    public interface ICategoryRepository
+    {
+        ICollection<Category> GetCategories();
+        Category GetCategory(int categoryId);
+        ICollection<Category> GetCategoriesOfABook(int bookId);
+        ICollection<Book> GetBooksForCategory(int categoryId);
+        bool CategoryExists(int categoryId);
+    }
+}
