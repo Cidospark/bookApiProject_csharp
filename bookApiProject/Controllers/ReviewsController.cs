dee317b550eabd00b997a6e39ed549c54ea4cdea
cidospark <cidospark27@gmail.com>
2020-01-01 15:18:58 +0100

    added more actions

--- bookApiProject/Controllers/ReviewsController.cs
+++ bookApiProject/Controllers/ReviewsController.cs
@@ -46,5 +46,94 @@ namespace bookApiProject.Controllers
             return Ok(reviewsDto);
         }
 
+
+
+        // api/reviews/reviewId
+        [HttpGet("{reviewId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200, Type = typeof(ReviewDto))]
+        public IActionResult GetReview(int reviewId)
+        {
+            if (!_reviewRepository.ReviewExists(reviewId))
+                return NotFound();
+
+            var review = _reviewRepository.GetReview(reviewId);
+
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var reviewDto = new ReviewDto()
+            {
+                Id = review.Id,
+                HeadLine = review.HeadLine,
+                ReviewText = review.ReviewText,
+                Rating = review.Rating
+            };
+
+            return Ok(reviewDto);
+        }
+
+
+        //api/reviews/books/bookId
+        [HttpGet("books/{bookId}")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<ReviewDto>))]
+        public IActionResult GetReviewsOfABook(int bookId)
+        {
+            // validate book
+
+            var reviews = _reviewRepository.GetReviewsOfABook(bookId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var reviewsDto = new List<ReviewDto>();
+            foreach(var review in reviews)
+            {
+                reviewsDto.Add(new ReviewDto()
+                {
+                    Id = review.Id,
+                    HeadLine = review.HeadLine,
+                    ReviewText = review.ReviewText,
+                    Rating = review.Rating
+                });
+            }
+
+            return Ok(reviewsDto);
+        }
+
+
+        //api/reviews/books/bookId
+        [HttpGet("books/{bookId}")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(200, Type = typeof(ReviewDto))]
+        public IActionResult GetBookOfAReview(int bookId)
+        {
+            // validate book
+
+            var reviews = _reviewRepository.GetReviewsOfABook(bookId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var reviewsDto = new List<ReviewDto>();
+            foreach (var review in reviews)
+            {
+                reviewsDto.Add(new ReviewDto()
+                {
+                    Id = review.Id,
+                    HeadLine = review.HeadLine,
+                    ReviewText = review.ReviewText,
+                    Rating = review.Rating
+                });
+            }
+
+            return Ok(reviewsDto);
+        }
+
+
     }
 }
